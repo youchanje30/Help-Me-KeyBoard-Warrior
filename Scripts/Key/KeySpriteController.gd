67337fdff44cb6d3c1ce4926ea1fea7d5dc36321
psk <77984774+youchanje30@users.noreply.github.com>
2025-01-08 23:21:03 +0900

    keyboard setting
    make keyboard with hand.. tired

--- Scripts/Key/KeySpriteController.gd
+++ Scripts/Key/KeySpriteController.gd
@@ -1,28 +1,42 @@
 extends Node
 
-
+@export var key_texture : Texture2D
 @export var sprite : Sprite2D
 
+var region_x_size : int = 0
+
+
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	set_sprite_region()
+
+#func _process(delta: float) -> void:
+	#pass
 
 
-func _process(delta: float) -> void:
-	pass
+func set_sprite_region():
+	sprite.region_enabled = true
+	if key_texture != null: sprite.texture = key_texture
+	var texture_size_x = sprite.texture.get_size().x
+	var texture_size_y = sprite.texture.get_size().y
+	region_x_size = texture_size_x / 3 # it always cut by 3
+	var region_rect_size = Vector2(region_x_size, texture_size_y)
+	sprite.region_rect.size = region_rect_size
+	sprite.region_rect.position = Vector2.ZERO
 
 
+
+#region Change Sprite
 func up_btn()->void:
-	sprite.region_rect.position = Vector2(82, 0)
-	pass
+	sprite.region_rect.position = Vector2(region_x_size*2, 0)
 
 func press_btn()->void:
-	sprite.region_rect.position = Vector2(41, 0)
-	pass
+	sprite.region_rect.position = Vector2(region_x_size*1, 0)
 
 func disable_btn()->void:
 	sprite.region_rect.position = Vector2.ZERO
-	pass
-
+#endregion
 
 var cnt = 0
 func _on_timer_timeout() -> void:
