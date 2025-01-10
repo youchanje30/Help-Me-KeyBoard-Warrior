7c0748bbe2631fb7624811d7d93f98b82ddcaed7
psk <77984774+youchanje30@users.noreply.github.com>
2025-01-10 19:19:47 +0900

    key detail update
    Key Moving Anim Update
    Fix Duplication input

--- Scripts/Keyboard/KeyboardController.gd
+++ Scripts/Keyboard/KeyboardController.gd
@@ -1,22 +1,36 @@
 extends Node
 
 var key_list : Array[key_controller]
+var pressed_key_list : Array[bool]
 
 func _ready() -> void:
 	key_list.resize(27)
+	pressed_key_list.resize(27)
+	pressed_key_list.fill(false)
 
 func add_key(index:int, key:key_controller):
 	key_list[index] = key
 
 func input_key(index: int, is_pressed : bool):
-	if is_pressed:
-		key_list[index].press_btn()
-		cur_key_list.append(index)
-	else:
-		key_list[index].up_btn()
+	# up btn has not good image
+	# just keeping press_btn is more better than before
+	
+	#if is_pressed:
+		#key_list[index].press_btn()
+		#cur_key_list.append(index)
+	#else:
+		#key_list[index].up_btn()
+	
+	if pressed_key_list[index]: return
+
+	key_list[index].press_btn()
+	pressed_key_list[index] = true
+	cur_key_list.append(index)
 
 
 var cur_key_list = []
 func reset_key():
 	for index in cur_key_list:
 		key_list[index].disable_btn()
+		pressed_key_list[index] = false
+	cur_key_list.resize(0)
--- Scripts/Managers/InputManager.gd
+++ Scripts/Managers/InputManager.gd
@@ -3,18 +3,15 @@ extends Node
 
 
 func _input(event: InputEvent) -> void:
-		
-	# input get 65 a, 90 z like ASCII Code
+	# input get 65 A, 90 Z like ASCII Code
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("ui_accept"):
 			KeyboardController.reset_key()
 			
 		if 65 <= event.keycode and event.keycode <= 90 and not event.echo: # a <= event.keycode <= z
 			#test_func(event.echo)
-			#print(, )
 			var key = event.keycode - 65
 			KeyboardController.input_key(key, event.is_pressed())
-	pass
 
 func _ready() -> void: pass
 
