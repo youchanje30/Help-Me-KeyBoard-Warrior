2fe6905658853207a9b31d0f1b6cd5b21938d3b1
psk <77984774+youchanje30@users.noreply.github.com>
2025-01-12 23:59:36 +0900

    ready to commands
    just ready..
    some effect

--- Scripts/Managers/EmenyController.gd
+++ Scripts/Managers/EmenyController.gd
@@ -2,12 +2,19 @@ extends Node
 
 var enemy_base = preload("res://Scenes/Enemys/BaseEnemy.tscn")
 var distance = 300
+
+var enemys : Array[IEnemy]
+
 func _ready() -> void:
 	
-	for i in range(10):	
+	for i in range(30):	
 		var spawn_pos = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 		var enemy = enemy_base.instantiate()
 		add_child(enemy)
-	
+		enemys.append(enemy)
 		enemy.global_position = spawn_pos * distance
 		enemy._ready()
+
+
+func get_random_enemy_pos():
+	return enemys[randi_range(0, enemys.size())].position
