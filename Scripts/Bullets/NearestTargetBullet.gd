ff05f59d3ebfb59105ac11679cfd5902c9290f45
psk <77984774+youchanje30@users.noreply.github.com>
2025-01-14 13:44:08 +0900

    Add new bullet Effect
    Add Nearest Auto Bullet
    add explosion effect

---
+++ Scripts/Bullets/NearestTargetBullet.gd
@@ -0,0 +1,14 @@
+extends IBullet
+
+var _force : float = 0
+var speed : float = 10
+var vec : Vector2 = Vector2.UP
+
+func shoot(angle : float=0, force : float=0, damage : float = 0):
+	super.shoot(angle, force, damage)
+	_force = force
+	var pos = EnemyController.get_nearest_enemy_pos()
+	vec = (pos - position).normalized()
+
+func move(delta):
+	translate(vec * delta * _force)# * speed 
--- Scripts/Managers/EnemyController.gd
+++ Scripts/Managers/EnemyController.gd
@@ -11,12 +11,25 @@ func _ready() -> void:
 		var enemy = enemy_base.instantiate()
 		add_child(enemy)
 		enemys.append(enemy)
-		enemy.global_position = spawn_pos * distance
+		enemy.global_position = spawn_pos * (distance + randf_range(-100, 100))
 		enemy._ready()
 
 
 func get_random_enemy_pos():
 	return enemys[randi_range(0, enemys.size()-1)].position
+
+func get_nearest_enemy_pos():
+	var min_pos = Vector2.ZERO
+	var min_distance = (enemys[0].position.x * enemys[0].position.x) + (enemys[0].position.y * enemys[0].position.y)
+	for enemy in enemys:
+		var x = enemy.position.x
+		var y = enemy.position.y
+		var cur_distance = x*x + y*y
+		if min_distance < cur_distance: continue
+		min_distance = cur_distance
+		min_pos = enemy.position
+	
+	return min_pos
 
 func Remove_Enemy(enemy):
 	enemys.erase(enemy)
