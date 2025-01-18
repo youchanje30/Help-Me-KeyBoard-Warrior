b3a7286652c40451b2400b005408d0934efb4736
psk <77984774+youchanje30@users.noreply.github.com>
2025-01-18 16:35:08 +0900

    jump move enemy
    add curve to move

--- Scripts/Enemys/JumpEnemy.gd
+++ Scripts/Enemys/JumpEnemy.gd
@@ -1,11 +1,17 @@
 extends IEnemy
 
 var move_vec : Vector2
-@export var speed : float = 3
+@export var speed : float = 50
+@export var speed_curve : Curve
+var cur_time : float
 
 func _ready() -> void:
 	super._ready()
 	move_vec = -self.global_position.normalized()
+	cur_time = 0
 
 func move(delta):
-	translate(move_vec * speed * delta)
+	var time = clampf(cur_time + delta, 0, 1)
+	var speed = speed_curve.sample(time)
+	translate(move_vec * 50 * speed * delta)
+	cur_time = time if time < 1 else 0
